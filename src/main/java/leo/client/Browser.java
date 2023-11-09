50dfc6eb09ba4a494afc768acd34e9f916704f77
Vedran Pehlivanovic <vedran.pehlivanovic@gmail.com>
2023-11-09 10:24:47 -0700

    Added url to error message, and fixed opening of browsers for macos a… (#10)
    * Added url to error message, and fixed opening of browsers for macos and linux systems
    
    * fixed formatting and added eclipse gitignore settings
    
    * formatting
    
    * fix EOF

--- src/main/java/leo/client/Browser.java
+++ src/main/java/leo/client/Browser.java
@@ -14,36 +14,33 @@ package leo.client;
 import javax.swing.*;
 import java.lang.reflect.Method;
 
-
 public class Browser {
     private static final String errMsg = "Error attempting to launch web browser";
 
     public static void open(String url) {
         String osName = System.getProperty("os.name");
-
+        Runtime runtime = Runtime.getRuntime();
         try {
             if (osName.startsWith("Mac OS")) {
-                Class fileMgr = Class.forName("com.apple.eio.FileManager");
-                Method openURL = fileMgr.getDeclaredMethod("openURL", String.class);
-                openURL.invoke(null, url);
-            } else if (osName.startsWith("Windows"))
-                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-            else {
-                //assume Unix or Linux
-                String[] browsers =
-                        {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
+                runtime.exec("open " + url);
+            } else if (osName.startsWith("Windows")) {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else {
+                // assume Unix or Linux
+                String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
                 String browser = null;
                 for (int count = 0; count < browsers.length && browser == null; count++)
-                    if (Runtime.getRuntime().exec(new String[]{"which", browsers[count]}).waitFor() == 0)
+                    if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0) {
                         browser = browsers[count];
-
-                if (browser == null)
-                    throw new Exception("Could not find web browser");
-                else
-                    Runtime.getRuntime().exec(new String[]{browser, url});
+                    }
+                if (browser == null) {
+                    throw new Exception(String.format("Could not find web browser.%nURL: %s", url));
+                } else {
+                    runtime.exec("xdg-open " + url);
+                }
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
         }
     }
-} 
+}
