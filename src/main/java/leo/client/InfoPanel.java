45f9ec8932580a6c68e3873d60eb6e38f4bfd0f7
Josiah Pisciotta <462_josiah@chroniclogic.com>
2025-02-13 20:24:46 -0800

    cleanup

--- src/main/java/leo/client/InfoPanel.java
+++ src/main/java/leo/client/InfoPanel.java
@@ -11,7 +11,7 @@ package leo.client;
 import leo.shared.Constants;
 
 import java.awt.*;
-import java.util.Vector;
+//import java.util.Vector;
 import java.util.regex.*;
 import java.util.List;
 import java.util.ArrayList;
